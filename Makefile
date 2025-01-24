38e59392e442af8a6d9a7a5aff3a9c1003783550
Mariusz Milejko <mariusz@milejko.pl>
2025-01-25 00:07:30 +0100

    nix-shell + stable kuick package

--- Makefile
+++ Makefile
@@ -16,6 +16,8 @@ test: version.txt
 	$(eval CI_TAG := $(IMAGE_NAME):$(shell date +%s%N))
 	docker build --target=test-runner --tag $(CI_TAG) .
 	docker run --rm -v ./:/var/www/html $(CI_TAG) sh -c "composer up && composer test:all"
+	# test project creation
+	docker run --rm $(CI_TAG) sh -c "composer create-project kuick/project:dev-main && composer test:all"
 	docker image rm $(CI_TAG)
 
 build: version.txt
---
+++ default.nix
@@ -0,0 +1,25 @@
+# [Nix](https://nixos.org) based development environment using PHP Serve
+# 1. Install Nix: `sh <(curl -L https://nixos.org/nix/install)`
+# 2. Run command: `nix-shell`
+
+{ pkgs ? import <nixpkgs> {}}:
+
+let myPhp = pkgs.php83.buildEnv {
+    extensions = ({ enabled, all }: enabled ++ [ all.xdebug ] ++ [ all.apcu ]);
+    extraConfig = ''
+        memory_limit=128M
+        upload_max_filesize=256M
+        post_max_size=256M
+    '';
+};
+in pkgs.mkShell {
+    packages = [
+        myPhp
+        myPhp.packages.composer
+    ];
+
+    shellHook = ''
+        composer up
+        composer start
+    '';
+}
